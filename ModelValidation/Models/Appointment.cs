983db66b49a020a8264761d611db019ab5bf216d
Anthony Leif <tonyleif@hotmail.com>
2017-11-28 21:16:05 -0600

    Page 686

--- ModelValidation/Models/Appointment.cs
+++ ModelValidation/Models/Appointment.cs
@@ -6,9 +6,10 @@ using ModelValidation.Infrastructure;
 namespace ModelValidation.Models
 {
     //[NoJoeOnMondays]
-    public class Appointment : IValidatableObject
+    public class Appointment //: IValidatableObject
     {
-        //[Required]
+        [Required]
+        [StringLength(10, MinimumLength = 3)]
         public string ClientName { get; set; }
 
         [DataType(DataType.Date)]
@@ -20,30 +21,30 @@ namespace ModelValidation.Models
         //[MustBeTrue(ErrorMessage = "You must accept the terms")]
         public bool TermsAccepted { get; set; }
 
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            List<ValidationResult> errors = new List<ValidationResult>();
-            if (string.IsNullOrEmpty(ClientName))
-            {
-                errors.Add(new ValidationResult("Please enter your name"));
-            }
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    List<ValidationResult> errors = new List<ValidationResult>();
+        //    if (string.IsNullOrEmpty(ClientName))
+        //    {
+        //        errors.Add(new ValidationResult("Please enter your name"));
+        //    }
 
-            if(DateTime.Now > Date)
-            {
-                errors.Add(new ValidationResult("Please enter a date in the future"));
-            }
+        //    if(DateTime.Now > Date)
+        //    {
+        //        errors.Add(new ValidationResult("Please enter a date in the future"));
+        //    }
 
-            if(errors.Count == 0 && ClientName == "Joe" && Date.DayOfWeek == DayOfWeek.Monday)
-            {
-                errors.Add(new ValidationResult("Joe cannot book appointments on Mondays"));
-            }
+        //    if(errors.Count == 0 && ClientName == "Joe" && Date.DayOfWeek == DayOfWeek.Monday)
+        //    {
+        //        errors.Add(new ValidationResult("Joe cannot book appointments on Mondays"));
+        //    }
 
-            if (!TermsAccepted)
-            {
-                errors.Add(new ValidationResult("You must accept the terms"));
-            }
+        //    if (!TermsAccepted)
+        //    {
+        //        errors.Add(new ValidationResult("You must accept the terms"));
+        //    }
 
-            return errors;
-        }
+        //    return errors;
+        //}
     }
 }
