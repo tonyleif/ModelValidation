f3325e1434b96b42764ae7b2c594fcf32148b4bc
Anthony Leif <tonyleif@hotmail.com>
2017-11-28 20:00:28 -0600

    Page 676

--- ModelValidation/Controllers/HomeController.cs
+++ ModelValidation/Controllers/HomeController.cs
@@ -29,6 +29,12 @@ namespace ModelValidation.Controllers
                 ModelState.AddModelError("TermsAccepted", "You must accept the terms");
             }
 
+            if(ModelState.IsValidField("ClientName") && ModelState.IsValidField("Date") 
+                && appt.ClientName == "Joe" && appt.Date.DayOfWeek == DayOfWeek.Monday)
+            {
+                ModelState.AddModelError("", "Joe cannot book appointments on Mondays");
+            }
+
             if (ModelState.IsValid)
             {
                 // statements to store new Appointment in a
--- ModelValidation/Models/Appointment.cs
+++ ModelValidation/Models/Appointment.cs
@@ -5,9 +5,14 @@ namespace ModelValidation.Models
 {
     public class Appointment
     {
+        [Required]
         public string ClientName { get; set; }
+
         [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Please enter a date")]
         public DateTime Date { get; set; }
+
+        [Range(typeof(bool), "true", "true", ErrorMessage = "You must accept the terms")]
         public bool TermsAccepted { get; set; }
     }
 }
