a572ec7129d69145c7584ddf416b50fe3c73e18f
Anthony Leif <tonyleif@hotmail.com>
2017-11-28 20:23:34 -0600

    Page 682

--- ModelValidation/Models/Appointment.cs
+++ ModelValidation/Models/Appointment.cs
@@ -5,19 +5,45 @@ using ModelValidation.Infrastructure;
 
 namespace ModelValidation.Models
 {
-    [NoJoeOnMondays]
-    public class Appointment
+    //[NoJoeOnMondays]
+    public class Appointment : IValidatableObject
     {
-        [Required]
+        //[Required]
         public string ClientName { get; set; }
 
         [DataType(DataType.Date)]
         //[Required(ErrorMessage = "Please enter a date")]
-        [FutureDate(ErrorMessage = "Please enter a date in the future")]
+        //[FutureDate(ErrorMessage = "Please enter a date in the future")]
         public DateTime Date { get; set; }
 
         //[Range(typeof(bool), "true", "true", ErrorMessage = "You must accept the terms")]
-        [MustBeTrue(ErrorMessage = "You must accept the terms")]
+        //[MustBeTrue(ErrorMessage = "You must accept the terms")]
         public bool TermsAccepted { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> errors = new List<ValidationResult>();
+            if (string.IsNullOrEmpty(ClientName))
+            {
+                errors.Add(new ValidationResult("Please enter your name"));
+            }
+
+            if(DateTime.Now > Date)
+            {
+                errors.Add(new ValidationResult("Please enter a date in the future"));
+            }
+
+            if(errors.Count == 0 && ClientName == "Joe" && Date.DayOfWeek == DayOfWeek.Monday)
+            {
+                errors.Add(new ValidationResult("Joe cannot book appointments on Mondays"));
+            }
+
+            if (!TermsAccepted)
+            {
+                errors.Add(new ValidationResult("You must accept the terms"));
+            }
+
+            return errors;
+        }
     }
 }
